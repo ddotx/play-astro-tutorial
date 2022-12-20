d453b35026f57679cccba801d8ce7c111645b7c2
ddotx_mbp <domemacintosh@gmail.com>
2022-12-20 22:55:26 +0700

    finished

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,20 @@
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+
+export default function Greeting({messages}) {
+
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+  
+  const [greeting, setGreeting] = useState(randomMessage());
+
+  return (
+    <div> 
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => {
+        console.log('clicked')
+        return setGreeting(randomMessage())}}>
+        New Greeting
+      </button>
+    </div>
+  );
+}
